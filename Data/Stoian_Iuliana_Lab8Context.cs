bda298cffdffec233c1cb46ca7c9560e0456985c
Iuliana, Stoian <iuliana@devopsland.club>
2022-01-03 18:10:33 +0200

    Add project files.

---
+++ Data/Stoian_Iuliana_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Stoian_Iuliana_Lab8.Models;
+
+namespace Stoian_Iuliana_Lab8.Data
+{
+    public class Stoian_Iuliana_Lab8Context : DbContext
+    {
+        public Stoian_Iuliana_Lab8Context (DbContextOptions<Stoian_Iuliana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Stoian_Iuliana_Lab8.Models.Book> Book { get; set; }
+    }
+}
